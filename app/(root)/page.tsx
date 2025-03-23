639fbdf1237bb27a92d85fdee0d433d69a468e5a
jackpk <pawankumarbairwa879@gmail.com>
2025-03-23 17:25:51 +0530

    interview card add

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,33 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import InterviewCard from "@/components/InterviewCard";
 function Home() {
+  const hasPastInterviews = 1;
+  const user = { id: "user123" };
+  const userInterviews = [
+    {
+      id: "1",
+      role: "Frontend Developer",
+      type: "Technical",
+      techstack: ["React", "JavaScript", "CSS"],
+      createdAt: "2025-03-20",
+    },
+    {
+      id: "2",
+      role: "Backend Developer",
+      type: "Behavioral",
+      techstack: ["Node.js", "Express", "MongoDB"],
+      createdAt: "2025-03-21",
+    },
+    {
+      id: "3",
+      role: "Full Stack Developer",
+      type: "System Design",
+      techstack: ["React", "Node.js", "AWS"],
+      createdAt: "2025-03-22",
+    },
+  ];
   return (
     <>
       <section className="card-cta">
@@ -26,16 +52,31 @@ function Home() {
       </section>{" "}
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
-        <div className="interview-section">
-          <p>You haven&apos;t taken any interview yet</p>
+
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
-      <section className="flex flex-col gap-6 mt-8">
+      {/* <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interview-section">
           <p>There are no inerview available</p>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,12 @@
 type FormType = "sign-in" | "sign-up";
+interface InterviewCardProps {
+  interviewId?: string;
+  userId?: string;
+  role: string;
+  type: string;
+  techstack: string[];
+  createdAt?: string;
+}
+interface TechIconProps {
+  techStack: string[];
+}
